ab8cdf37f67eef1828f7d2131fde6a104598f8c1
Stanley So <stanley.c.so@gmail.com>
2019-02-19 07:00:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // add solution here
 
-function theBeatlesPlay ()
+function theBeatlesPlay (musicians_arr, instruments_arr) {
+  
+  let strings_arr = [];
+  
+  for (i = 0; i < musicians_arr.length; i++) {
+    strings_arr.push(musicians_arr[i] + " plays " + instruments_arr[i]);
+  }
+ 
+  return strings_arr; 
+}
+
